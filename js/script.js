e286a9a2fb5a8ef53deece4ed8482d67ac5f5c8c
Samuel <svaldehita@gmail.com>
2025-01-31 09:10:15 +0100

    Añadir archivos iniciales del proyecto:
    - Creado index.html con estructura básica
    - Añadido css/style.css para estilos
    - Añadido js/script.js para funcionalidad

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Evento que se ejecuta al cargar el contenido del DOM
+document.addEventListener('DOMContentLoaded', function (event) {
+
+    // Constantes del DOM
+
+    // Variables
+
+    // Eventos
+
+    // Funciones
+
+})
